c313f33ecc706b8324e1a72ad776f9270bae8f55
Cédric Luthi <cedric.luthi@gmail.com>
2024-05-23 22:40:11 +0200

    Use file-scoped namespaces

--- test/Xunit.SkippableFact.Tests/SkipTests.cs
+++ test/Xunit.SkippableFact.Tests/SkipTests.cs
@@ -1,84 +1,83 @@
 ﻿// Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the Microsoft Public License (Ms-PL). See LICENSE.txt file in the project root for full license information.
 
-namespace Xunit.SkippableFact.Tests
+namespace Xunit.SkippableFact.Tests;
+
+public class SkipTests
 {
-    public class SkipTests
+    [Fact]
+    public void If()
     {
-        [Fact]
-        public void If()
-        {
-            Assert.Throws<SkipException>(() => Skip.If(true));
-            Skip.If(false);
-        }
+        Assert.Throws<SkipException>(() => Skip.If(true));
+        Skip.If(false);
+    }
 
-        [Fact]
-        public void If_WithReason()
+    [Fact]
+    public void If_WithReason()
+    {
+        string reason = "some reason";
+        try
         {
-            string reason = "some reason";
-            try
-            {
-                Skip.If(true, reason);
-                Assert.Fail("If should have thrown.");
-            }
-            catch (SkipException ex)
-            {
-                Assert.Equal(reason, ex.Message);
-            }
+            Skip.If(true, reason);
+            Assert.Fail("If should have thrown.");
         }
-
-        [Fact]
-        public void IfNot()
+        catch (SkipException ex)
         {
-            Assert.Throws<SkipException>(() => Skip.IfNot(false));
-            Skip.IfNot(true);
+            Assert.Equal(reason, ex.Message);
         }
+    }
 
-        [Fact]
-        public void IfNot_WithReason()
+    [Fact]
+    public void IfNot()
+    {
+        Assert.Throws<SkipException>(() => Skip.IfNot(false));
+        Skip.IfNot(true);
+    }
+
+    [Fact]
+    public void IfNot_WithReason()
+    {
+        string reason = "some reason";
+        try
         {
-            string reason = "some reason";
-            try
-            {
-                Skip.IfNot(false, reason);
-                Assert.Fail("IfNot should have thrown.");
-            }
-            catch (SkipException ex)
-            {
-                Assert.Equal(reason, ex.Message);
-            }
+            Skip.IfNot(false, reason);
+            Assert.Fail("IfNot should have thrown.");
         }
-
-        [Fact]
-        public void If_SupportsNullableReferenceTypesPostCondition()
+        catch (SkipException ex)
         {
-            // Provoke a possibly null value that is not detectable through
-            // static analysis
-            string? value =
-                int.Parse("42", System.Globalization.CultureInfo.InvariantCulture) == 42
+            Assert.Equal(reason, ex.Message);
+        }
+    }
+
+    [Fact]
+    public void If_SupportsNullableReferenceTypesPostCondition()
+    {
+        // Provoke a possibly null value that is not detectable through
+        // static analysis
+        string? value =
+            int.Parse("42", System.Globalization.CultureInfo.InvariantCulture) == 42
                 ? "Not null"
                 : null;
 
-            Skip.If(value is null);
+        Skip.If(value is null);
 
-            // Does not trigger a nullable reference type warning
-            _ = value.Substring(0);
-        }
+        // Does not trigger a nullable reference type warning
+        _ = value.Substring(0);
+    }
 
-        [Fact]
-        public void IfNot_SupportsNullableReferenceTypesPostCondition()
-        {
-            // Provoke a possibly null value that is not detectable through
-            // static analysis
-            string? value =
-                int.Parse("42", System.Globalization.CultureInfo.InvariantCulture) == 42
+    [Fact]
+    public void IfNot_SupportsNullableReferenceTypesPostCondition()
+    {
+        // Provoke a possibly null value that is not detectable through
+        // static analysis
+        string? value =
+            int.Parse("42", System.Globalization.CultureInfo.InvariantCulture) == 42
                 ? "Not null"
                 : null;
 
-            Skip.IfNot(value is object);
+        Skip.IfNot(value is object);
 
-            // Does not trigger a nullable reference type warning
-            _ = value.Substring(0);
-        }
+        // Does not trigger a nullable reference type warning
+        _ = value.Substring(0);
     }
 }
